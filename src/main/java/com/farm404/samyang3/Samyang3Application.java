7c781dcac836b0c84819b653f5a8a085b762ef23
ParkMyeongChelo_박명철 <shalompmc0505@naver.com>
2025-08-01 00:50:08 +0900

    Fix compilation errors, enhance CSS styling, and configure Spring Boot 3.3.0

--- src/main/java/com/farm404/samyang3/Samyang3Application.java
+++ src/main/java/com/farm404/samyang3/Samyang3Application.java
@@ -1,9 +1,11 @@
 package com.farm404.samyang3;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.farm404.samyang3.mapper")
 public class Samyang3Application {
 
 	public static void main(String[] args) {
